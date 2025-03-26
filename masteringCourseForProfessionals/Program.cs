7f579827ad6668d13183b783f434b44008ef1f9c
Aly Mamdouh <aly.mamdouh@interactts.com>
2025-03-26 11:50:58 +0200

    Add project files.

---
+++ masteringCourseForProfessionals/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace masteringCourseForProfessionals
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
