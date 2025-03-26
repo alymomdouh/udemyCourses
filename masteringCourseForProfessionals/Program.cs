2f87b8f2e98da1c765bf3d7706949467cc506bad
Aly Mamdouh <aly.mamdouh@interactts.com>
2025-03-26 12:57:26 +0200

    Generic classes

--- masteringCourseForProfessionals/Program.cs
+++ masteringCourseForProfessionals/Program.cs
@@ -5,6 +5,19 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello, World!");
+
+            var mySampleInt = new Sample<int>();
+            mySampleInt.Field = 1;
+            Console.WriteLine(mySampleInt.ShowInfo());
+
+
+
+            var mySampleChar = new Sample<char>();
+            mySampleChar.Field = 'A';
+            Console.WriteLine(mySampleChar.ShowInfo());
+
+            Console.WriteLine("Hello, World!");
         }
     }
 }
---
+++ masteringCourseForProfessionals/Sample.cs
@@ -0,0 +1,23 @@
+﻿namespace masteringCourseForProfessionals
+{
+    internal class Sample<T>
+    {
+        T field;
+        public T Field
+        {
+            get
+            {
+                return field;
+            }
+            set
+            {
+                field = value;
+            }
+        }
+        public string ShowInfo()
+        {
+            // return $"Type: {typeof(T).Name}, Value: {field}";
+            return $"Type: {field.GetType()}, Value: {field}";
+        }
+    }
+}
