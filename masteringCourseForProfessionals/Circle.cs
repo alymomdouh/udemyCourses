30b88cfcea103f8092b628280f056ebbab67494d
Aly Mamdouh <aly.mamdouh@interactts.com>
2025-03-27 12:22:59 +0200

    9. Delegates, 10. Delegate multicasting 11. Delegate multicasting points

---
+++ masteringCourseForProfessionals/Circle.cs
@@ -0,0 +1,37 @@
+﻿namespace masteringCourseForProfessionals
+{
+    public class Circle
+    {
+        /*
+        public delegate void CircleDelegate(double radius);
+        public void CalculateArea(double radius)
+        {
+            Console.WriteLine($"Circle Area is \"{radius * radius * Math.PI}\"");
+        }
+        public void CalculatePrimeter(double radius)
+        {
+            Console.WriteLine($"Circle Primeter is \"{2 * radius * Math.PI}\"");
+        }
+        public void ShowInfo(double radius)
+        {
+            Console.WriteLine($"Circle with radius \"{radius}\"");
+        }
+        */
+        public delegate int CircleDelegate(double radius);
+        public int CalculateArea(double radius)
+        {
+            Console.WriteLine($"Circle Area is \"{radius * radius * Math.PI}\"");
+            return 2;
+        }
+        public int CalculatePrimeter(double radius)
+        {
+            Console.WriteLine($"Circle Primeter is \"{2 * radius * Math.PI}\"");
+            return 1;
+        }
+        public int ShowInfo(double radius)
+        {
+            Console.WriteLine($"Circle with radius \"{radius}\"");
+            return 0;
+        }
+    }
+}
--- masteringCourseForProfessionals/Program.cs
+++ masteringCourseForProfessionals/Program.cs
@@ -1,6 +1,6 @@
 ﻿namespace masteringCourseForProfessionals
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
@@ -35,10 +35,10 @@
             var calc3 = new Calculator<double>();
             Console.WriteLine($"Addition result :{calc3.Add(12.4, 10.7)}");
 
-            var calc4 = new Calculator<char>();
-            Console.WriteLine($"Addition result :{calc4.Add('A', 'B')}");
+            //var calc4 = new Calculator<char>();
+            //Console.WriteLine($"Addition result :{calc4.Add('A', 'B')}");
 
-            ShowValueAndType<dynamic>(calc4);
+            // ShowValueAndType<dynamic>(calc4);
 
             void ShowValueAndType<TVariable>(TVariable variable)
             {
@@ -70,7 +70,15 @@
                 var result = x - y;
                 showSumResult(result);
             }
+            void Multiply(int x, int y)
+            {
+                Console.WriteLine($"Result of {x}* {y} is => :{x * y}");
 
+            }
+            void Add(int x, int y)
+            {
+                Console.WriteLine($"Result of {x}+ {y} is => :{x + y}");
+            }
             var show = delegate (int result)
             {
                 Console.WriteLine($"Result is => :{result}");
@@ -79,7 +87,73 @@
             SubTract(40, 10, show);
             show(50);
 
+            var cal = new CalculateDelegate((x, y) =>
+            {
+                Console.WriteLine($"Result is => :{x + y}");
+            });
+
+            var calDelegate1 = new CalculateDelegate(Multiply);
+            calDelegate1(10, 20);
+            var calDelegate2 = new CalculateDelegate(Add);
+            calDelegate2(10, 20);
+            calDelegate2(33, 20);
+            calDelegate2(12, 12);
+            calDelegate2.Invoke(12, 12);// equal or same result as calDelegate2(12, 12);
+
+            //  delegate multi casting 
+
+            var multcast = new CalculateDelegate(Add);
+            multcast += Multiply;
+            multcast(10, 20);
+            multcast -= Multiply;
+
+
+            var circle = new Circle();
+            var circleDelegate = new Circle.CircleDelegate(circle.ShowInfo);
+            circleDelegate += circle.CalculateArea;
+            circleDelegate += circle.CalculatePrimeter;
+            var delegateResult = circleDelegate.Invoke(10);
+            //delegateResult will be the last method return value in the multi casting here is CalculatePrimeter
+            circleDelegate -= circle.CalculatePrimeter;
+            var delegateResult2 = circleDelegate(10);// delegateResult2= CalculateArea
+            circleDelegate -= circle.ShowInfo;
+            circleDelegate -= circle.CalculateArea;
+            circleDelegate -= circle.CalculatePrimeter;
+            if (circleDelegate != null)
+            {
+                circleDelegate(10);
+            }
+            else
+            {
+                Console.WriteLine("Delegate is null");
+            }
+            /*
+               what is a delegate?
+            -An object that refers to a method
+               Why Use Delegates?
+            1- Encapsulate a methods.
+            2- used for Callbacks methods and events.
+            3-can add one or more methods to a delegate instance or single event click.
+            4- can be used for calling anonymous methods. 
+             
+            5- Pass methods as parameters.
+            6- Declare a method signature.
+            7- can remove one or more methods from a delegate instance or single event click.
+            8-Asynchronous programming.
+
+            delegate multi casting 
+            -A delegate can point to multiple methods from single call of delegate 
+            -all the added methods will be executed in order FIFO
+            - Add methods to a delegate using + or += operator  
+            - remove methods to a delegate using - or -= operator  
+            important note  delegate multi casting should have void return type 
+
+
+             */
+
             Console.WriteLine("Hello, World!");
         }
+        public delegate void CalculateDelegate(int x, int y);
+
     }
 }
